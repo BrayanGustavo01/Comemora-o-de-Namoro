546d1fc27bd13e3ebacbf9ac89cbaeb785410d35
Gustavo <143816558+BrayanGustavo01@users.noreply.github.com>
2024-12-11 22:00:30 -0300

    Add files via upload

---
+++ site-momo/script.js
@@ -0,0 +1,18 @@
+
+
+setInterval(countdown, 1000);
+
+// Animação de rolagem
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.3 });
+
+fadeElements.forEach(el => observer.observe(el));
+
+
